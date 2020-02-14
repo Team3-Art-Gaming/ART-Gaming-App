c4ab06eef657de74e225fa65380d9d9ce8f854d3
kevinwp55 <47406156+kevinwp55@users.noreply.github.com>
2020-02-14 15:45:15 -0800

    Linked Login,Reg,Verify
    Linked together 3 scenes that are meant to replicate webpage screens on mobile app.

---
+++ Assets/Scripts/navControllerTester.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class navControllerTester : MonoBehaviour
+{
+    public void sceneLoader(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
