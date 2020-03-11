d0046d47a403c6798b7734e028fcf1550cc564e2
Gerry300 <48077359+Gerry300@users.noreply.github.com>
2020-03-10 21:34:23 -0700

    The Addition of UI Menus
    All UI Menus made by Gerardo has transferred safely, and will only commit once, then return all current work over to UI Branch once again.

---
+++ Assets/Pop_Up_Screen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pop_Up_Screen : MonoBehaviour
+{
+    [SerializeField]
+    Text textbox;
+    
+    [SerializeField]
+    Button ok_Button;
+    
+    [SerializeField]
+    Button cancel_Button;
+
+    private GameObject caller;
+    
+    public void activatePopUp(string s, GameObject caller)
+    {
+        this.caller = caller;
+        transform.position = new Vector3(0,0,0);
+        textbox.text = s;
+    }
+
+    public void deactivatePopUp()
+    {
+            transform.position = new Vector3(-10000, -10000, 0);
+    }
+
+    public void okay_Click()
+    {
+        caller.SendMessage("POP_UP_RESPONSE", "OK");
+    }
+
+    public void cancel_Click()
+    {
+        caller.SendMessage("POP_UP_RESPONSE", "Cancel");
+    }
+}
