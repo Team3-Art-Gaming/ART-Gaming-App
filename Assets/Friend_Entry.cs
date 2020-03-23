3f0b5e05fafc3edfbdffeed6ca82fea4476f4657
Gerry300 <48077359+Gerry300@users.noreply.github.com>
2020-03-22 18:35:40 -0700

    Friend List Initialization
    With this new branch, I will continue to develop the friend list alongside my partners, and making sure that all assets are up to date, and in preparations for the testing for the next few weeks.

---
+++ Assets/Friend_Entry.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Friend_Entry : MonoBehaviour
+{
+    [SerializeField] 
+    Text Friend_Name;
+    [SerializeField] 
+    Image Friend_Icon;
+    [SerializeField] 
+    Button Send_Button;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Friend_List.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Friends
+{
+    public string Name;
+    public string Status; 
+    public Friends(string name, string status)
+    {
+        Name = name;
+        Status = status; 
+    }
+}
+
+public class Friend_List : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Image FriendPrefab;
+    [SerializeField]
+    Image Grid;
+    
+    public List<Friends> friends;
+    void Start()
+    {
+        friends = new List<Friends>();
+        Friends F = new Friends("John", "Pending");
+        friends.Add(F);
+        F = new Friends("Alice", "Requesting");
+        friends.Add(F);
+        F = new Friends("Mike", "Pending");
+        friends.Add(F);
+        F = new Friends("Oscar", "Requesting");
+        friends.Add(F);
+        F = new Friends("Diana", "Friend");
+        friends.Add(F);
+        F = new Friends("Max", "Friend");
+        friends.Add(F);
+        F = new Friends("George", "Friend");
+        friends.Add(F);
+        F = new Friends("James", "Requesting");
+        friends.Add(F);
+        F = new Friends("Hector", "Pending");
+        friends.Add(F);
+        F = new Friends("Jessie", "Requesting");
+        friends.Add(F);
+        F = new Friends("Ashley", "Pending");
+        friends.Add(F);
+
+        foreach(Friends friend in friends)
+        {
+            Image I = Instantiate(FriendPrefab, Grid.transform);
+            I.SendMessage("SetName", friend.Name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
