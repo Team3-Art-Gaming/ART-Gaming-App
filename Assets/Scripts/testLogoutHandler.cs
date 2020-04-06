4db3c13d8549a08645e6aa227612dfb73e580890
kevinwp55 <47406156+kevinwp55@users.noreply.github.com>
2020-04-06 14:36:00 -0700

    bug fixed maps and profile buttons
    maps btn now directs to maps scene
    profile btn now directs to user profile
    added sign out functionality

---
+++ Assets/Scripts/testLogoutHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Extensions;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine.SceneManagement;
+
+public class testLogoutHandler : MonoBehaviour
+{
+
+    public void authSignOut()
+    { 
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.GetAuth(Firebase.FirebaseApp.DefaultInstance);
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        Debug.LogFormat("User: " + PlayerPrefs.GetString("Username") + " with email: " + user.Email + " signing out");
+        auth.SignOut();
+        PlayerPrefs.SetString("Username", "");
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(0);
+    }
+    
+}
