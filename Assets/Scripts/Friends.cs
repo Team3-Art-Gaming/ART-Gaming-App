2ae4102809945d9caf61ce2e283834c03f7dd472
Gerry300 <48077359+Gerry300@users.noreply.github.com>
2020-04-15 18:27:29 -0700

    Launch Screen Script Added and Grid Issue

--- Assets/Scripts/Friends.cs
+++ Assets/Scripts/Friends.cs
@@ -6,11 +6,13 @@ public class Friends
 {
     public string Name;
     public string Status;
+    public bool isInvited;
 
-    public Friends(string name, string status)
+    public Friends(string name, string status, bool invited = false)
     {
         this.Name = name;
         this.Status = status;
+        this.isInvited = invited;
     }
 
 }
---
+++ Assets/Scripts/Launch_Screen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Launch_Screen : MonoBehaviour
+{
+    [SerializeField]
+    Button Home;
+    [SerializeField]
+    Button Launch;
+    [SerializeField]
+    Image Grid; 
+    [SerializeField] 
+    Image Grid_Prefab;
+
+    public List<Image> Prefab;
+    public List<Friends> friends;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.friends.Add(new Friends("friend1","Friend"));
+        this.friends.Add(new Friends("friend2", "Friend"));
+        this.friends.Add(new Friends("friend3", "Friend"));
+
+        foreach (Friends f in friends)
+        {
+            Image I = Instantiate(Grid_Prefab, Grid.transform); 
+            //I.SendMessage("SetName", friend);
+            Prefab.Add(I);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
